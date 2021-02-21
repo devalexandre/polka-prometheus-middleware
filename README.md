2e47f42629753ba307a157e1acf4f0a701642aa4
User <user@192.168.1.3>
2021-02-21 14:13:32 -0300

    feat: beta version

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # polka-prometheus-middleware
 polka middleware to prometheus
+
+```.javascript
+
+const promMid = require('polka-prometheus-middleware')
+
+const app  = promMid({
+    metricsPath: '/metrics',
+    collectDefaultMetrics: true,
+    requestDurationBuckets: [0.1, 0.5, 1, 1.5]
+})
+
+app
+    .get('/users/:id', (req, res) => {
+        console.log(`~> Hello, ${req.hello}`);
+        res.end(`User: ${req.params.id}`);
+    })
+    .listen(3000, err => {
+        if (err) throw err;
+        console.log(`> Running on localhost:3000`);
+    });
+```
---
+++ src/metrics.js
@@ -0,0 +1,32 @@
+const Prometheus = require('prom-client');
+
+/**
+ * @param prefix - metrics name prefix
+ * request counter
+ */
+function requestCountGenerator(labelNames, prefix = '') {
+    return new Prometheus.Counter({
+        name: `${prefix}http_requests_total`,
+        help: 'Counter for total requests received',
+        labelNames,
+    });
+}
+
+/**
+ * @param {!Array} buckets - array of numbers, representing the buckets for
+ * @param prefix - metrics name prefix
+ * request duration
+ */
+function requestDurationGenerator(labelNames, buckets, prefix = '') {
+    return new Prometheus.Histogram({
+        name: `${prefix}http_request_duration_seconds`,
+        help: 'Duration of HTTP requests in seconds',
+        labelNames,
+        buckets,
+    });
+}
+
+module.exports = {
+    requestCountGenerator,
+    requestDurationGenerator,
+};
